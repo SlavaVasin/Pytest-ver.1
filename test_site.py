935ea9422e5870822d46f1268707750b11448679
Slava Vasin <inlite001@gmail.com>
2025-03-30 21:42:33 +0300

    add implicitly_wait(3)

--- test_site.py
+++ test_site.py
@@ -5,13 +5,14 @@ import pytest
 
 @pytest.fixture()
 def driver():
-    browser = webdriver.Chrome()
+    browser = webdriver.Firefox()
     browser.maximize_window()
+    browser.implicitly_wait(3)
     yield browser
 
 def test_open_s6(driver):
     driver.get('https://www.demoblaze.com/')
-    galaxy_s6 = driver.find_element(By.LINK_TEXT,'Samsung galaxy s6')
+    galaxy_s6 = driver.find_element(By.XPATH,'//a[text()="Samsung galaxy s6"]')
     galaxy_s6.click()
     title = driver.find_element(By.CSS_SELECTOR, 'h2')
     assert title.text == 'Samsung galaxy s6'
