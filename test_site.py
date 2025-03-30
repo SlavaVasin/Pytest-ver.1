a6f41805c7bc7e67d74785a8b2f017df09abd020
Slava Vasin <inlite001@gmail.com>
2025-03-30 21:32:10 +0300

    add def test_s6

--- test_site.py
+++ test_site.py
@@ -1,5 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
 
 
-def test_open_s6():
-    pass
+@pytest.fixture()
+def driver():
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    yield browser
+
+def test_open_s6(driver):
+    driver.get('https://www.demoblaze.com/')
+    galaxy_s6 = driver.find_element(By.LINK_TEXT,'Samsung galaxy s6')
+    galaxy_s6.click()
+    title = driver.find_element(By.CSS_SELECTOR, 'h2')
+    assert title.text == 'Samsung galaxy s6'
